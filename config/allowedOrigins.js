9d7d4fb680fdd23bb03982e9a4b07ffa17ecbbca
Braulio Mercado <b_mercado@mail.fhsu.edu>
2024-05-08 16:36:31 -0400

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://inf653-final-proj.onrender.com'
 ];
 
 // Export the array of allowed origins
