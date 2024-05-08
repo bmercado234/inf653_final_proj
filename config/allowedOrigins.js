4d112aa9d42af0761e858c5e06919805954e06e1
Braulio Mercado <b_mercado@mail.fhsu.edu>
2024-05-08 16:51:16 -0400

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,10 @@
 // Define an array of allowed origins
 const allowedOrigins = [
+    'https://dazzling-snickerdoodle-777101.netlify.app/',
+    'https://inf653-final-proj.onrender.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000',
-    'https://inf653-final-proj.onrender.com'
+    'http://localhost:3000'
 ];
 
 // Export the array of allowed origins
